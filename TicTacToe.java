c5038361e5887af167915e13656ad79f117af07c
parag <adhauparag64@gmail.com>
2020-08-23 01:14:16 +0530

    Added : Functionality of begin with a toss to

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,12 @@
+import java.util.Random;
+import java.util.*;
 public class TicTacToe{
          static String[] board;
         public static void main(String args []){
+             Scanner sc = new Scanner(System.in);
+             System.out.print("Select your choice (1:Head, 2:Tail): ");
+             int userChoice = sc.nextInt();
+             toss(userChoice);
              board=new String[9];
              populateEmptyBoard();
              printBoard();
@@ -20,4 +26,14 @@ public class TicTacToe{
 			board[a] = String.valueOf(a+1);
 		}
 	}
-   }
+        static void toss(int userChoice){
+               Random r = new Random();
+               int randomChoice = r.nextInt(2)+1;
+               if( randomChoice == userChoice ){
+                   System.out.println("Player win the toss and player will play first");
+                  }
+               else{
+                   System.out.println("Computer win the toss and computer will play first");
+                  }
+             }
+         }
