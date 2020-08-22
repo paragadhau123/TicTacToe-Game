552958efb073067d6ac09b49c203ae2fe47550b5
parag <adhauparag64@gmail.com>
2020-08-23 00:55:00 +0530

    Added : Start fresh with resetting the board

---
+++ TicTacToe.java
@@ -0,0 +1,23 @@
+public class TicTacToe{
+         static String[] board;
+        public static void main(String args []){
+             board=new String[9];
+             populateEmptyBoard();
+             printBoard();
+   }
+   static void printBoard() {
+		System.out.println("/---|---|---\\");
+		System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+		System.out.println("/---|---|---\\");
+	}
+
+	static void populateEmptyBoard() {
+		for (int a = 0; a < 9; a++) {
+			board[a] = String.valueOf(a+1);
+		}
+	}
+   }
